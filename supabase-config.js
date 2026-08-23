c785721b54679efb98a035c39feaae674e55fad2
Noeline Gaikwad <noelinegaikwad29@gmail.com>
2026-08-23 16:35:24 +0530

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://muirfynmhktmxvsmfizf.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_VJyHMEQXElbs3qYqK9LhGQ_29G8iuIF";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
